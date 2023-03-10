3d86639a587c1bdb2f53355cebc0694f291fa722
thaisglc <thaisgleite@gmail.com>
2023-03-10 12:34:08 -0300

    helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,28 +1,53 @@
 package helpers
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
+	"net/http"
 	"strings"
 )
 
 // Manda o Usuario para validar na API de LOGON
 func CheckUserPass(username, password string) bool {
-	userpass := make(map[string]string)
-	userpass["hello"] = "itsme"
-	userpass["john"] = "doe"
+	requestData := map[string]string{
+		"nome":  username,
+		"senha": password,
+	}
 
-	log.Println("checkUserPass", username, password, userpass)
-
-	if val, ok := userpass[username]; ok {
-		log.Println(val, ok)
-		if val == password {
-			return true
-		} else {
-			return false
-		}
-	} else {
+	//Marshal the Json data to bytes
+	requestDataBytes, err := json.Marshal(requestData)
+	if err != nil {
+		log.Println(err)
 		return false
 	}
+
+	// create a new http request
+	requestBody, err := http.NewRequest("GET", "https://r9jv3rrmsw.us-east-1.awsapprunner.com/logon", bytes.NewBuffer(requestDataBytes))
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	//set the Content-Type header to application/json
+	requestBody.Header.Set("Content-Type", "application/json")
+
+	//send the request using the default Http Client
+	resp, err := http.DefaultClient.Do(requestBody)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	var response string
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	log.Println(response)
+	return true
 }
 
 func EmptyUserPass(username, password string) bool {
